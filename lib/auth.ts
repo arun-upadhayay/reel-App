685ff9b5166165ceef3e69c354a147ea665cd454
arun-upadhayay <arunupadhayay10102000@gmail.com>
2025-02-25 23:16:19 +0530

    create all next auth related file and its code

---
+++ lib/auth.ts
@@ -0,0 +1,86 @@
+import bcrypt from "bcrypt";
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectionToDatabase } from "./db";
+import Users from "@/models/Users";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+        },
+        email: {
+          label: "Email",
+          type: "text",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        if (
+          !credentials?.email ||
+          !credentials?.password ||
+          !credentials?.username
+        ) {
+          throw new Error("Email, Password and Username are required");
+        }
+
+        try {
+          await connectionToDatabase();
+          const user = await Users.findOne({ email: credentials.email });
+
+          if (!user) {
+            throw new Error("User not found");
+          }
+
+          const isValid = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+
+          if (!isValid) {
+            throw new Error("Invalid password");
+          }
+
+          return {
+            id: user._id.toString(),
+            email: user.email,
+            username: user.username,
+          };
+        } catch (error) {
+          throw error;
+        }
+      },
+    }),
+  ],
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+
+    async session({ session, token }) {
+      if (session?.user) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+    signOut: "/login",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
---
+++ middleware.ts
@@ -0,0 +1,31 @@
+import withAuth from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware(req) {
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      async authorized({ token, req }) {
+        const { pathname } = req.nextUrl;
+        if (
+          pathname.startsWith("/api/auth") ||
+          pathname.startsWith("/login") ||
+          pathname.startsWith("/register")
+        ) {
+          return true;
+        }
+        //public routes
+        if (pathname.startsWith("/") || pathname.startsWith("/api/videos")) {
+          return true;
+        }
+
+        return !!token;
+      },
+    },
+  }
+);
+
+
+export const config = { matcher: ["/((?!_next/static|_next/image|favicon.ico|public).*)"] };
